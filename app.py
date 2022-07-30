a7e2e471bd2a37c7a72cdabd885e6543b154c697
badapple21 <mjn2024@gmail.com>
2022-07-29 21:28:26 -0400

    Problems with algo and js updating

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from flask import Flask, redirect, render_template, request, session, json
 from flask_session import Session
 from solver import solve, format
 
-#configure application
+# configure application
 app = Flask(__name__)
 
-#Ensure auto-reloaded
+# Ensure auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/fetch")
 def fetch():
@@ -24,15 +26,16 @@ def fetch():
     y = request.args.get("y")
 
     if q and w and e and r and t and y:
-        status, coefficient1, coefficient2, charge1, charge2, reactant1, reactant2 = solve(q, w, e, r, t, y)
-        if(status=="Error"):
-            rtnHTML="Could not calculate"
+        app.logger.error(q, w, e, r, t, y)
+        status, coefficient1, coefficient2, charge1, charge2, reactant1, reactant2 = solve(
+            q, w, e, r, t, y)
+        if(status == "Error"):
+            rtnHTML = "Could not calculate"
         else:
-            rtnHTML = format(coefficient1, coefficient2, charge1, charge2, reactant1, reactant2, t, y)
+            rtnHTML = format(coefficient1, coefficient2, charge1,
+                             charge2, reactant1, reactant2, t, y)
 
     else:
-        rtnHTML = "Error - enter all felids" 
-
+        rtnHTML = "Error - enter all felids"
 
     return rtnHTML
-
--- solver.py
+++ solver.py
@@ -1,11 +1,14 @@
 from math import fabs
 
+
 def solve(q, w, e, r, t, y):
     reactant1 = q
     charge1 = fabs(int(w))
+    print(charge1)
 
     reactant2 = e
     charge2 = fabs(int(r))
+    print(charge2)
 
     amount1 = int(t)
     amount2 = int(y)
@@ -15,31 +18,29 @@ def solve(q, w, e, r, t, y):
 
     ratio = value1 / value2
     solved = False
-    while solved==False:
-        max = 3
-        for i in range(1, max+1):
-            for j in range(1, max+1):
-                if i*amount1==charge1 and j*amount2==charge2:
+    while solved == False:
+        max = 300
+        for i in range(0, max+1):
+            for j in range(0, max+1):
+                print(f"")
+                if (i*amount1 == charge1) and (j*amount2 == charge2):
                     return "Solved", i, j, value1, value2, reactant1, reactant2
-        if(max<100):
-            max+=1
+        if(max < 100):
+            max += 1
         else:
-            return "Error", "-1", "-1" ,"-1", "-1", "-1", "-1"
+            return "Error", "-1", "-1", "-1", "-1", "-1", "-1"
+
 
 def format(coefficient1, coefficient2, charge1, charge2, reactant1, reactant2, amount1, amount2):
 
-    if(coefficient1==1):
+    if(coefficient1 == 1):
         coefficient1 = ""
-    if(coefficient2==1):
-        coefficient2=""
-    if(amount1==1):
-        amount1=""
-    if(amount2==1):
-        amount2=""
+    if(coefficient2 == 1):
+        coefficient2 = ""
+    if(amount1 == 1):
+        amount1 = ""
+    if(amount2 == 1):
+        amount2 = ""
 
     HTML = f"{coefficient1}{reactant1}<sub>{int(amount1)}</sub> + {coefficient2}{reactant2}<sub>{int(amount2)}</sub> -> {reactant1}<sub>{int(charge1)}</sub>{reactant2}<sub>{int(charge2)}</sub>"
     return HTML
-
-
-
-
